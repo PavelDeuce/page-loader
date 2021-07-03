1852b645f74fb0a2b8e29c412317be1a24869afb
PavelDeuce <deucela.pavel@gmail.com>
2021-07-03 22:19:28 +0300

    commander fix output

--- src/commander.js
+++ src/commander.js
@@ -1,16 +1,19 @@
-import commander from 'commander';
+import { Command } from 'commander';
 
 import loadPage from '.';
 import { version } from '../package.json';
 
+const program = new Command();
+
 export default () => {
-  commander
+  return program
     .version(version)
     .description('Download the webpage by url')
     .arguments('<url>')
-    .option('-o', '--output [path]', process.cwd())
-    .action((url) => {
-      loadPage(url, commander.output)
+    .option('-o --output [path]', 'output directory', process.cwd())
+    .action((url, argv) => {
+      console.log(argv);
+      loadPage(url, argv.output)
         .then((fileName) => console.log(`The page was downloaded to ${url} as ${fileName}`))
         .catch((err) => {
           console.error(err.message);
