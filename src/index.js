a37b9c287c55ad1178a620c2e9b3390b81989cbe
PavelDeuce <deucela.pavel@gmail.com>
2021-07-09 22:46:40 +0300

    refactoring and fixed notions by cooliukh

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 import path from 'path';
 import axios from 'axios';
+import debug from 'debug';
 import { promises as fs } from 'fs';
+import map from 'lodash/map';
+import Listr from 'listr';
 
 import changeLinksToRelative from './parser.js';
-import loadAllResources from './resources-service.js';
 import { createLinkPath, linkTypesMapping } from './utils.js';
-import log from './logger.js';
+
+const log = debug('page-loader');
 
 export default (requestUrl, outputPath = process.cwd()) => {
-  const htmlPath = path.join(outputPath, createLinkPath(requestUrl, linkTypesMapping.html));
+  const loadAllResources = (links, resourcesDirectory) => {
+    const loadResource = (link) => {
+      const responseType = 'arraybuffer';
+      return axios
+        .get(link, { responseType })
+        .then(({ data }) => {
+          const fileName = createLinkPath(link);
+          log(`The file ${fileName} was successfully loaded to ${resourcesDirectory}`);
+          return fs.writeFile(path.join(resourcesDirectory, fileName), data);
+        })
+        .catch((error) => {
+          log(`Fetch resource ${link} failed with message: ${error.message}`);
+          throw error;
+        });
+    };
+
+    return fs
+      .mkdir(resourcesDirectory)
+      .then(() => {
+        const listrOptions = { exitOnError: false, concurrent: true };
+        const listrData = map(links, (link) => ({
+          title: `Loading ${link}`,
+          task: () => loadResource(link),
+        }));
+        const tasks = new Listr(listrData, listrOptions);
+        return tasks.run();
+      })
+      .catch((error) => {
+        log(`Folder creating ${resourcesDirectory} failed with message: ${error.message}`);
+        throw error;
+      });
+  };
 
   return axios.get(requestUrl).then((res) => {
     log(`Loading the page ${requestUrl} to ${outputPath}`);
+    const htmlPath = path.join(outputPath, createLinkPath(requestUrl, linkTypesMapping.html));
     const { links, updatedHtml } = changeLinksToRelative(res.data, requestUrl);
     return fs
       .writeFile(htmlPath, updatedHtml)
--- src/logger.js
+++
@@ -1,5 +0,0 @@
-import debug from 'debug';
-
-const log = debug('page-loader');
-
-export default log;
